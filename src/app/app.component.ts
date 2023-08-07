8fa9c92049f51b6b1c12ce30801892c046af5548
Izabela <iszybiak.kontakt@gmail.com>
2023-08-07 11:27:13 +0200

    addModal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,53 @@ import {ProductService} from "./product.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  public products: Product[] = [];
-  constructor(private productService: ProductService) {}
+  public products: Product[] ;
+  // public editProduct: Product;
+  // public deleteProduct: Product;
+
+  constructor(private productService: ProductService){
+    this.products = [];
+  }
 
   ngOnInit() {
     this.getProducts();
   }
 
-  public getProducts(): void{
-    this.productService.getProducts().subscribe(
-      (response: Product[]) => {
+  public getProducts(): void {
+    this.productService.getProducts().subscribe({
+      next: (response: Product[]) => {
         this.products = response;
+        console.log(this.products);
       },
-      (error: HttpErrorResponse)=> {
+      error: (error: HttpErrorResponse) => {
         alert(error.message);
       }
-    )
+    });
   }
+
+  public onOpenModal(product: Product | null, mode: string): void {
+    const container = document.getElementById('mainContainer');
+    if (container !== null) {
+      const button = document.createElement('button');
+      button.type = 'button';
+      button.style.display = 'none';
+      button.setAttribute('data-toggle', 'modal');
+      if (mode === 'add') {
+        console.log(document.getElementById('addProductModal'))
+        button.setAttribute('data-target', '#addProductModal');
+      }
+      if (mode === 'edit') {
+        console.log("Edit")
+        button.setAttribute('data-target', '#editProductModal');
+      }
+      if (mode === 'delete') {
+        button.setAttribute('data-target', '#deleteProductModal');
+      }
+      container.appendChild(button);
+      button.click();
+    }else {
+      console.log("Błąd")
+    }
+  }
+
 }
